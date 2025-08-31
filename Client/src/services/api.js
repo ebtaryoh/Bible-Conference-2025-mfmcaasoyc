14cf9987e62779e682dc86eb26c11234fd50788a
Ibitayo Akinnibosun <ibitayo.akinnibosun@gmail.com>
2025-08-31 01:52:23 +0100

    updated commit

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://bible-conference-2025-mfmcaasoyc.vercel.app/api",
 });
 
 // Attendees
